70964d2b3750a8af8095011fd37d0461f68e39cb
Ptench <marina@MacBook-Pro-Yumagulov.local>
2017-05-15 14:55:45 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { render } from 'react-dom';
+import ShopBag from './shopping_bag/ShopBag'
+
+
+
+render(<ShopBag />, document.getElementById('root'));
